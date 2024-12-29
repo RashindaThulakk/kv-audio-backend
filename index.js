df5abc57a1425dc10da10bd74c13879fa7adaaf5
RashindaThulakkana <rashindathulakkana36@gmail.com>
2024-12-29 14:24:14 +0530

    connected mongodb

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express"
 import bodyparser from "body-parser"
+import mongoos from "mongoos"
 
 const app = express()
 app.use(bodyparser.json())
+
+let mongoUrl = "mongodb+srv://admin:123@cluster0.gzhw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoos connect (mongoUrl)
+
+let connection = mongoos.connection
+connection.once("open",()=>{
+    console.log("mongodb connection established successfully")
+})
 
 app.get("/",
     (req,res)=>{
