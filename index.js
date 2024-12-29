d336294846756e6779a657d48479f669d0bfd200
RashindaThulakkana <rashindathulakkana36@gmail.com>
2024-12-29 07:38:32 +0530

    get request calling

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express"
-
-
 const app = express()
+
+
+app.get("/",(req,res)=>{
+    console.log("This is a get request")
+})
+
+
+
+
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
 })
 
 
-console.log("Hello Rashinda")
