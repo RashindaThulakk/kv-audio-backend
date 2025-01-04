cc62ad077ede2d3acb334dd495aed11f8641963f
RashindaThulakkana <rashindathulakkana36@gmail.com>
2025-01-04 17:26:16 +0530

    "message": "Student saved successfully" 😎😎

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-import express from "express"
-import bodyparser from "body-parser"
-import mongoos from "mongoos"
+import express from "express";
+import bodyparser from "body-parser";
+import mongoose from "mongoose";
+
+
+
 
 const app = express()
 app.use(bodyparser.json())
 
-let mongoUrl = "mongodb+srv://admin:123@cluster0.gzhw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-mongoos connect (mongoUrl)
+let mongoUrl = "mongodb+srv://admin:123@cluster0.gzhw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-let connection = mongoos.connection
-connection.once("open",()=>{
-    console.log("mongodb connection established successfully")
-})
+// //mongoose.connect(mongoUrl);
+
+// const connection = mongoose.connection;
+
+// connection.once("open",() => 
+//     {
+//     console.log("mongodb connection established successfully")
+// })
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => 
+    console.log("MongoDB connection established successfully"))
+  .catch
+  ((err) => console.error("MongoDB connection error:", err));
+
+
 
 app.get("/",
     (req,res)=>{
@@ -22,6 +37,41 @@ app.get("/",
         {message: "Hello world "+req.body.name}
     )
 })
+
+app.post("/",
+    (req,res)=>{
+    let studentSchema = mongoose.Schema({
+        name : String,
+        age : Number,
+        height : Number,
+    });
+    let Student = mongoose.model("students",studentSchema)
+
+    let newStudent = req.body
+    let student = new Student(newStudent)
+
+    student.save().then(
+        ()=>{
+            res.json(
+                {
+                    "message" : "Student saved successfully"
+                }
+            )
+        }
+        .catch(
+            ()=>{
+                res.json(
+                    {
+                        "message" : "Student could not be saved"
+                    }
+                )
+            }
+        )
+
+    )
+
+
+});
 
 
 app.listen(3000,()=>{
