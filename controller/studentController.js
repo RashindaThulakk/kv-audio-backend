acccb8e8bdeb13fda1138a52959aff6780a00054
RashindaThulakkana <rashindathulakkana36@gmail.com>
2025-02-16 10:27:51 +0530

    fixed errors🎃

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,11 +1,5 @@
 export function getStudents(req,res){
     res.json({
-        message : "This is a get request from Student route"
-    })
-}
-
-export function postStudents(req,res){
-    res.json({
-        message : "This is a post request from Student route"
+        message : "This is a get Request from students route"
     })
 }
--- index.js
+++ index.js
@@ -1,54 +1,13 @@
 import express from "express";
-import bodyparser from "body-parser";
-import mongoose from "mongoose";
-import Student from "./models/student.js";
-import studentRouter from "./routes/studentRoute";
+
+const app = express();
 
 
-const app = express()
-app.use(bodyparser.json())
-app.use("/students",studentRouter)
-
-let mongoUrl = "mongodb+srv://admin:123@cluster0.gzhw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(mongoUrl);
-
-const connection = mongoose.connection;
-
-connection.once("open",() => 
-    {
-    console.log("mongodb connection established successfully")
-})
-
-mongoose
-  .connect(mongoUrl)
-  .then(() => 
-    console.log("MongoDB connection established successfully"))
-  .catch
-  ((err) => console.error("MongoDB connection error:", err));
+app.get("/", () => {
+  console.log("That is a request");
+});
 
 
-
-
-
-// app.get("/",(req,res)=>{
-//     student.find().then(
-//         (result)=> {
-//             res.json(result)
-// })
-//             .catch(
-//                 ()=> {
-//                     res.json({
-//                         message : "error occured"
-//                     })
-//                 }
-//             )
-// });
-
-
-
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
-})
-
-
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
