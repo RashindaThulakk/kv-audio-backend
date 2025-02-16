703cbd8da51f3a314743da4ddda62a059d51ee91
RashindaThulakkana <rashindathulakkana36@gmail.com>
2025-02-16 12:45:45 +0530

    database connection established successfully. 🎁 2nd video completed

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
 import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 
+app.use(bodyParser.json());
 
-app.get("/", () => {
-  console.log("That is a request");
+let mongoUrl = "mongodb+srv://admin:123@cluster0.gzhw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(mongoUrl)
+let connection = mongoose.connection
+connection.once("open",()=>{
+  console.log("MongoDB connection established successfully")
+})
+
+app.get("/", (req,res) => {
+  console.log("That is a get request");
+  res.json({ message : "Hello world"});
 });
+
+app.post("/",(req,res) =>{
+  let studentSchema = mongoose.Schema({
+    name : String,
+    age : Number,
+    height : Number
+  })
+ 
+  let Student = mongoose.model("students", studentSchema)
+
+  let newStudent = req.body
+
+  let student = new Student(newStudent)
+
+  student.save().then(
+    ()=>{
+      res.json(
+        {
+          "message" : "Student saved successfully"
+        }
+      )
+    }
+  ).catch(
+    ()=>{
+      res.json(
+        {
+          "message" : "Student could not be found"
+        }
+      )
+    }
+  )
+
+
+
+} )
+
+app.delete("/", (req,res)=>{
+  console.log("that is a delete request")
+})
 
 
 app.listen(3000, () => {
